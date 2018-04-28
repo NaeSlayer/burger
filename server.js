fb3a5ccfef994554367d4273ab55751047f5f1a1
lanae <rlfoard@gmail.com>
2018-04-28 14:06:59 -0700

    Test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8089;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
